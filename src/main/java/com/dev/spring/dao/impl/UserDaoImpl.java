c64f5e28a2ef7453629d1008822be30a217df338
Mykyta Arkhanhelskyi <nickarchangel97@gmail.com>
2020-06-02 12:42:40 +0300

    added User model, dao and service layers for it, using Spring Framework (#1)
    * added User model, dao and service layers for it, using Spring Framework
    
    * made add method void

---
+++ src/main/java/com/dev/spring/dao/impl/UserDaoImpl.java
@@ -0,0 +1,58 @@
+package com.dev.spring.dao.impl;
+
+import com.dev.spring.dao.UserDao;
+import com.dev.spring.exception.DataProcessingException;
+import com.dev.spring.model.User;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private static final Logger LOGGER = Logger.getLogger(UserDaoImpl.class);
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            LOGGER.info("User with id " + user.getId() + " was added to DB");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert User entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(User.class);
+            criteriaQuery.from(User.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get list of users", e);
+        }
+    }
+}
---
+++ src/main/java/com/dev/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.dev.spring.service.impl;
+
+import com.dev.spring.dao.UserDao;
+import com.dev.spring.model.User;
+import com.dev.spring.service.UserService;
+import com.dev.spring.util.HashUtil;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void add(User user) {
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(user.getPassword(), user.getSalt()));
+        userDao.add(user);
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+}
